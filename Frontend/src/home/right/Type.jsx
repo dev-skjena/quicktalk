d24b893725a2c6b79ea363d7f8f2a5f34a4ae189
Santosh Jena <litsantoshjena2017@gmail.com>
2024-09-02 14:50:23 +0530

    updated type component

--- Frontend/src/home/right/Type.jsx
+++ Frontend/src/home/right/Type.jsx
@@ -1,24 +1,37 @@
-import React from "react";
-import { IoSendSharp } from "react-icons/io5";
+import React, { useState } from "react";
+import { IoSend } from "react-icons/io5";
+import useSendMessage from "../../context/useSendMessage.js";
 
-const Type = () => {
+function Type() {
+  const [message, setMessage] = useState("");
+  const { loading, sendMessages } = useSendMessage();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await sendMessages(message);
+    setMessage("");
+  };
+
   return (
     <>
-      <div className="flex space-x-3 h-[8vh] text-center bg-gray-800">
-        <div className="w-[70%] mx-4 flex items-center ">
-          <input
-            type="text"
-            placeholder="Type here"
-            className="border-[1px] border-gray-700 w-full py-3 px-3 rounded-xl grow outline-none bg-slate-900"
-          />
+      <form onSubmit={handleSubmit}>
+        <div className="flex space-x-3 h-[8vh] text-center bg-gray-800">
+          <div className="w-[70%] mx-4">
+            <input
+              type="text"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder="Type here"
+              className="border-[1px] border-gray-700  flex items-center w-full py-3 px-3 rounded-xl grow outline-none bg-slate-900 mt-1"
+            />
+          </div>
+          <button className="text-3xl">
+            <IoSend />
+          </button>
         </div>
-
-        <button className="text-3xl">
-          <IoSendSharp />
-        </button>
-      </div>
+      </form>
     </>
   );
-};
+}
 
 export default Type;
